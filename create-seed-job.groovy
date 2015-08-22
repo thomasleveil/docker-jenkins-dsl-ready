afb6efd0bbd0bc5f309766bd1edb36d7e7c62503
Thomas LEVEIL <thomasleveil@gmail.com>
2015-08-22 14:18:16 +0000

    The SeedJob is only created if missing

--- create-seed-job.groovy
+++ create-seed-job.groovy
@@ -1,35 +1,39 @@
 import jenkins.model.*
 
 def jobName = "SeedJob"
-def configXml = '''<?xml version='1.0' encoding='UTF-8'?>
-<project>
-  <actions/>
-  <description></description>
-  <keepDependencies>false</keepDependencies>
-  <properties>
-  </properties>
-  <scm class="hudson.scm.NullSCM"/>
-  <canRoam>true</canRoam>
-  <disabled>false</disabled>
-  <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
-  <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
-  <triggers/>
-  <concurrentBuild>false</concurrentBuild>
-  <builders>
-    <javaposse.jobdsl.plugin.ExecuteDslScripts plugin="job-dsl@1.37">
-      <targets>dsl/*.groovy</targets>
-      <usingScriptText>false</usingScriptText>
-      <ignoreExisting>false</ignoreExisting>
-      <removedJobAction>IGNORE</removedJobAction>
-      <removedViewAction>IGNORE</removedViewAction>
-      <lookupStrategy>JENKINS_ROOT</lookupStrategy>
-      <additionalClasspath></additionalClasspath>
-    </javaposse.jobdsl.plugin.ExecuteDslScripts>
-  </builders>
-  <publishers/>
-  <buildWrappers/>
-</project>'''
+def configXml = '''\
+  <?xml version='1.0' encoding='UTF-8'?>
+  <project>
+    <actions/>
+    <description></description>
+    <keepDependencies>false</keepDependencies>
+    <properties>
+    </properties>
+    <scm class="hudson.scm.NullSCM"/>
+    <canRoam>true</canRoam>
+    <disabled>false</disabled>
+    <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
+    <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
+    <triggers/>
+    <concurrentBuild>false</concurrentBuild>
+    <builders>
+      <javaposse.jobdsl.plugin.ExecuteDslScripts plugin="job-dsl@1.37">
+        <targets>dsl/*.groovy</targets>
+        <usingScriptText>false</usingScriptText>
+        <ignoreExisting>false</ignoreExisting>
+        <removedJobAction>IGNORE</removedJobAction>
+        <removedViewAction>IGNORE</removedViewAction>
+        <lookupStrategy>JENKINS_ROOT</lookupStrategy>
+        <additionalClasspath></additionalClasspath>
+      </javaposse.jobdsl.plugin.ExecuteDslScripts>
+    </builders>
+    <publishers/>
+    <buildWrappers/>
+  </project>
+'''.stripIndent()
 
-def xmlStream = new ByteArrayInputStream( configXml.getBytes() )
-def seedJob = Jenkins.instance.createProjectFromXML(jobName, xmlStream)
-seedJob.scheduleBuild(0, null)
+if (!Jenkins.instance.getItem(jobName)) {
+  def xmlStream = new ByteArrayInputStream( configXml.getBytes() )
+  def seedJob = Jenkins.instance.createProjectFromXML(jobName, xmlStream)
+  seedJob.scheduleBuild(0, null)
+}
