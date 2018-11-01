bac4d60c453a9b729c0b19a9ff86718745915711
Thomas LEVEIL <thomasleveil@gmail.com>
2018-11-01 21:33:51 +0100

    do not nag with Jenkins setup wizard

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -3,5 +3,8 @@
 # update plugins
 /opt/bin/update_jenkins_plugins.py
 
+# do not nag with Jenkins setup wizard
+export JAVA_OPTS="${JAVA_OPTS} -Djenkins.install.runSetupWizard=false" 
+
 # start Jenkins
 exec /sbin/tini -- /usr/local/bin/jenkins.sh "$@"
